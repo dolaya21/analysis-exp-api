184b13b46fe489699d6431ca56b3975cdfc11b96
dolaya@smu.edu <dolaya@smu.edu>
2025-05-04 23:27:25 -0500

    Add user endpoint

---
+++ src/main/java/org/smu/controller/UserController.java
@@ -0,0 +1,36 @@
+package org.smu.controller;
+
+import org.smu.database.entity.User;
+import org.smu.database.repository.UserRepository;
+import org.smu.dto.UserRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody UserRequestDTO request) {
+        if (userRepository.existsById(request.getUsername())) {
+            return ResponseEntity.badRequest().body("User with this username already exists.");
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setCountryOfBirth(request.getCountryOfBirth());
+        user.setCountryOfResidence(request.getCountryOfResidence());
+        user.setAge(request.getAge());
+        user.setGender(request.getGender());
+        user.setVerified(request.getVerified());
+
+        userRepository.save(user);
+        return ResponseEntity.ok("User created successfully.");
+    }
+}
---
+++ src/main/java/org/smu/dto/UserRequestDTO.java
@@ -0,0 +1,15 @@
+package org.smu.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequestDTO {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String countryOfBirth;
+    private String countryOfResidence;
+    private Integer age;
+    private String gender;
+    private Boolean verified;
+}
